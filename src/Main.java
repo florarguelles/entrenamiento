faaddec1981ba5d977958051119c606f7e7f78bb
Flor Arguelles alvarez <1000070445@hexaware.com>
2022-10-26 12:12:37 -0500

    Mi primer archivo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+
+public class Main {
+
+    //Metodos, funciones, variables y constructores
+    public static void main(String[] args) {
+        System.out.println("Este es mi primer codigo!");
+    }
+}
